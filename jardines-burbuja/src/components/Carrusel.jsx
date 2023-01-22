679b6c15717b2592f89ff10ea44502eda407b24d
carlos Palla <carlosezequiel19palladino@gmail.com>
2023-01-22 20:25:41 -0300

    agregando carrusel

---
+++ jardines-burbuja/src/components/Carrusel.jsx
@@ -0,0 +1,35 @@
+<figure id="banner">
+    <article id="carousel-container" class="carousel-container">
+        <div class="mySlides animate">
+            <img src="../../public/images/fiesta-jardin.jpg" alt="slide" />
+
+        </div>
+        <div class="mySlides animate">
+        </div>
+
+        <div class="mySlides animate">
+            <img src="../../public/images/imagen-jardin.jpg" alt="slide" />
+        </div>
+
+        <div class="mySlides animate">
+
+        </div>
+
+        <div class="mySlides animate">
+            <img src="/images/1.png" alt="slide" />
+        </div>
+
+        {/* <!-- Next and previous buttons --> */}
+        <a class="prev" onclick="prevSlide()">&#10094;</a>
+        <a class="next" onclick="nextSlide()">&#10095;</a>
+
+        {/* <!-- The dots/circles --> */}
+        <div class="dots-container">
+            <span class="dots" onclick="currentSlide(1)"></span>
+            <span class="dots" onclick="currentSlide(2)"></span>
+            <span class="dots" onclick="currentSlide(3)"></span>
+            <span class="dots" onclick="currentSlide(4)"></span>
+            <span class="dots" onclick="currentSlide(5)"></span>
+        </div>
+    </article>
+</figure>
