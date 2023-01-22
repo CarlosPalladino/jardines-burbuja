f427819d7f4817adeaafff54dcdc77d593339537
carlos Palla <carlosezequiel19palladino@gmail.com>
2023-01-22 18:55:41 -0300

    llegando a la vista 1024

--- jardines-burbuja/src/pages/Home.jsx
+++ jardines-burbuja/src/pages/Home.jsx
@@ -8,18 +8,18 @@ export default function Navbar() {
       <Header />
       <section className='texts'>
         <i className="fa-solid fa-house icons"></i>
-        <article>
+        <article className="article">
           <h2>Nuestra institucion</h2>
           <p> Brindamos a nuestros alumnos una educación integral y formación de las pirmeras enseñanzas</p>
           <img src="../../public/images/imagen-jardin.jpg"  alt="jardin" className='image' width="350" />
         </article>
-        <article>
+        <article className="article">
         <h2>Actividades</h2>
         <p> Contamos con un ambiente cuidado para que cada actividad se realice 100% seguro </p>
         <img src="../../public/images/patio-de-juegos-del-jardin.jpg" className='image' width="350" />
         <i className="fa-solid fa-graduation-cap icons"></i>
         </article>
-        <article> 
+        <article className="article"> 
         <h2> Nuestros objetivos</h2>
         <p>
           Ofrecer una educación formal, incorporada a la enseñanza oficial, que los forme como sujetos competentes en donde se favorezca el desarrollo de sus habilidades para acceder a mejores condiciones de vida,
